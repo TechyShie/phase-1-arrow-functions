1f27e64b637398a714491252926f9bf5c9106d9c
TechyShie <susanshie316@gmail.com>
2025-06-07 20:42:19 +0300

    Completed arrow function lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square that takes one parameter and multiplies it times itself
+const square = x => x * x;
+
+// Arrow function called add that takes two parameters and adds them together
+const add = (a, b) => a + b;
